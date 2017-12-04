efcef09bbc989df81fe1d83dc1f11b6e548901b8
Steven Cox <scox@renci.org>
2017-12-03 20:58:46 -0500

    integrate apache spark

--- docker/datacommons_docker_setup.centos7.sh
+++ docker/datacommons_docker_setup.centos7.sh
@@ -66,3 +66,16 @@ pip install stars
 git clone https://github.com/stevencox/cwltool.git
 cd cwltool && pip install . && cd $VENV
 pip install jupyter
+
+sudo yum install -y krb5-devel
+pip install sparkmagic
+set -x
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+sparkmagic_path=$(pip show sparkmagic | grep -i location | sed -e "s,.*: ,,")
+cd $sparkmagic_path
+jupyter-kernelspec install sparkmagic/kernels/sparkkernel --user
+jupyter-kernelspec install sparkmagic/kernels/pysparkkernel --user
+jupyter-kernelspec install sparkmagic/kernels/pyspark3kernel --user
+jupyter-kernelspec install sparkmagic/kernels/sparkrkernel --user
+mkdir /home/dockeruser/.sparkmagic
+cp /home/dockeruser/sparkmagic.config.json /home/dockeruser/.sparkmagic/config.json
