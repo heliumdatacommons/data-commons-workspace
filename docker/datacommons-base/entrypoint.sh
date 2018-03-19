96b7ecf2acd28564f7515222bb94a50b44aeed13
Chris Ball <christopherball@rti.org>
2018-03-19 14:31:10 -0400

    [Issue#13] Add toil specific run commands

--- docker/datacommons-base/entrypoint.sh
+++ docker/datacommons-base/entrypoint.sh
@@ -75,14 +75,25 @@ if ! mount | grep "irodsFs.*/irods"; then
     irodsFs -onocache -o allow_other /renci/irods
 fi
 
-# for Davrods
+# start apache for webdav (port 80)
 sudo /usr/sbin/httpd &
 
+# For Toil
+sudo mkdir -p /var/log/datacommons
+sudo chown -R dockeruser:datacommons /var/log/datacommons
+toillog=/var/log/datacommons/toillog
+touch ${toillog}
+
+# Determine which virtual env to start at run time
 if [ "$1" == "venv" ] || [ -z "$1" ]; then
     # interactive
     cd ~/venv && source bin/activate
     bash -i
-else
+elif [ "$1" == "toilvenv" ]; then
+    toilvenv
+elif [ "$1" == "_toil_worker" ]; then
+    /usr/bin/_toil_worker "${@:2}"
+elif [ "$1" == "_cwl_worker" ]; then
     # run commands in virtualenvironment
     cd ~/venv && source bin/activate
     bash -c "$@"
