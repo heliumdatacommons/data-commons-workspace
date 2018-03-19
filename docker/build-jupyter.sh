52f78e4f8e89ca8dfaafa8dfe5569d54eabc477f
Chris Ball <christopherball@rti.org>
2018-03-19 15:31:26 -0400

    [Issue#13] Seperate entrypoint commands

--- docker/build-jupyter.sh
+++ docker/build-jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 imagename="heliumdatacommons/datacommons-jupyter"
 
-docker build -t "$imagename" -f ./datacommons-jupyter/Dockerfile.jupyter "$@" ./datacommons-jupyter
+docker build -t "$imagename" -f ./datacommons-jupyter/Dockerfile "$@" ./datacommons-jupyter
