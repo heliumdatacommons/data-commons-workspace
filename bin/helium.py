23bf6d19545c1b06086923229a2cb374fab5de26
Kyle Ferriter <kylerferriter@gmail.com>
2018-06-18 20:36:51 -0400

    can authenticate to irods backend in containers by using openid access token in place of password. Can run docker in the containers.

---
+++ bin/helium.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+import sys
+import os
+import argparse
+import subprocess
+
+# defaults
+CHRONOS_URL = 'https://stars-app.renci.org/chronos'
+IRODS_HOST = 'test.commonsshare.org'
+IRODS_PORT = '1247'
+IRODS_ZONE = 'commonssharetestZone'
+IRODS_CWD = '/'+IRODS_ZONE
+IRODS_HOME = '/'+IRODS_ZONE
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="Helpful launcher script for Helium Data Commons containers")
+    
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
+    #subcommand build
+    build_parser = subparsers.add_parser('build', help='Build an image')
+    build_parser.add_argument('image', type=str, choices=['all','base','jupyter'])
+    
+
+    #subcommand run
+    run_parser = subparsers.add_parser('run', help='Run an image')
+    run_subparsers = run_parser.add_subparsers(title='run-subcommands', dest='image')
+
+    # options specific to the base image
+    base_parser = run_subparsers.add_parser('base', help='run the datacommons-base image')
+    entry_group = base_parser.add_mutually_exclusive_group(required=True)
+    entry_group.add_argument('--cwl', action='store_true',
+            help='Enters the cwltool virtualenvironment')
+    entry_group.add_argument('--toil', action='store_true',
+            help='Enters the toil virtual environment')
+
+    # options specific to the jupyter image
+    jupyter_parser = run_subparsers.add_parser('jupyter', help='run the datacommons-jupyter image')
+    jupyter_entry_group = jupyter_parser.add_mutually_exclusive_group(required=True)
+    jupyter_entry_group.add_argument('--wes-server', action='store_true', help='attach to the WES server process')
+    jupyter_entry_group.add_argument('--jupyter', action='store_true', help='attach to the Jupyter notebook process')
+    jupyter_entry_group.add_argument('--venv', action='store_true', help='enter Jupyter virtual environment')
+
+    # general run parser args
+    def add_general_run_args(parser):
+        parser.add_argument('-U', '--username', dest='username', required=True, help='Username to connect to iRODS.')
+        creds_group = parser.add_mutually_exclusive_group(required=True)
+        creds_group.add_argument('-P', '--password', type=str, default=None, help='Password to authenticate with.')
+        creds_group.add_argument('-T', '--access-token', type=str, default=None, help='Access token to authenticate with.')
+    
+        parser.add_argument('--irods-host', type=str, default=IRODS_HOST, help='iRODS host to connect to.')
+        parser.add_argument('--irods-port', type=str, default=IRODS_PORT, help='iRODS port to connect to.')
+        parser.add_argument('--irods-zone', type=str, default=IRODS_ZONE, help='iRODS zone to connect to.')
+        parser.add_argument('--irods-home', type=str, default=IRODS_HOME, help='iRODS home to use.')
+        parser.add_argument('--irods-cwd', type=str, default=IRODS_CWD, help='iRODS collection to mount.')
+        parser.add_argument('--chronos-url', type=str, default=CHRONOS_URL, help='Chronos instance to send jobs to.')
+        parser.add_argument('--openid-provider', type=str, help='When using access token, specify provider, otherwise uses default provider')
+    add_general_run_args(base_parser)
+    add_general_run_args(jupyter_parser)
+
+    options = parser.parse_args(argv)
+
+    #print(str(options))
+    # couldn't find a good way to automatically require subcommands, check for them here
+    if options.subcommand is None:
+        parser.print_help()
+        exit(1)
+    elif options.subcommand == 'build':
+        if options.image is None:
+            build_parser.print_help()
+            exit(1)
+        build(options)
+    elif options.subcommand == 'run':
+        if options.image is None:
+            run_parser.print_help()
+            exit(1)
+        run(options)
+    else:
+        raise RuntimeError('Unrecognized subcommand')
+
+def build(options):
+    os.chdir('docker')
+    imagename = options.image
+    build_args = ['/usr/bin/bash', 'build-{}.sh'.format(imagename)]
+    proc = subprocess.Popen(build_args, stdout=subprocess.PIPE, universal_newlines=True)
+    for line in proc.stdout:
+        print(line, end='')
+    ret = proc.wait()
+    print('build({} returned status_code: {}'.format(imagename, ret))
+
+def run(options):
+    env = {}
+    if options.chronos_url: env['CHRONOS_URL'] = options.chronos_url
+    if options.irods_host: env['IRODS_HOST'] = options.irods_host
+    if options.irods_port: env['IRODS_PORT'] = options.irods_port
+    if options.irods_zone: env['IRODS_ZONE_NAME'] = options.irods_zone
+    if options.irods_home: env['IRODS_HOME'] = options.irods_home
+    if options.irods_cwd: env['IRODS_CWD'] = options.irods_cwd
+    if options.username: env['IRODS_USER_NAME'] = options.username
+    if options.password: env['IRODS_PASSWORD'] = options.password
+    if options.access_token:
+        env['IRODS_AUTHENTICATION_SCHEME'] = 'openid'
+        env['IRODS_ACCESS_TOKEN'] = options.access_token
+        if 'IRODS_PASSWORD' in env: del(env['IRODS_PASSWORD'])
+    # turns a dictionary of env var to val into a list of [ '-e', 'k=v', '-e', 'k2=v2', ... ]
+    env_args = [val for tuple in zip(['-e' for k in env], ["{}={}".format(k,v) for k,v in env.items() if env[k]]) for val in tuple]
+    
+    run_args = [
+        'docker', 'run', '-it', '--privileged', '--rm',
+        '--name', 'dc_{}'.format(options.image), '-p', '90:80'] \
+        + env_args \
+        + ['heliumdatacommons/datacommons-{}'.format(options.image)]
+    if options.toil:
+        run_args.append('toilvenv')
+    elif options.cwl:
+        run_args.append('venv')
+    print(run_args)
+    print('COMMAND STRING: \n{}'.format(' '.join(run_args)))
+    proc = subprocess.Popen(run_args, universal_newlines=True)
+    #for line in proc.stdout:
+    #    print(line)
+    ret = proc.wait()
+    print('run({} returned status_code: {}'.format(options.image, ret))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- docker/datacommons-base/base-start.sh
+++ docker/datacommons-base/base-start.sh
@@ -20,38 +20,37 @@ fi
 
 # start dockerd
 sudo /usr/bin/dockerd-current --add-runtime docker-runc=/usr/libexec/docker/docker-runc-current --default-runtime=docker-runc  --userland-proxy-path=/usr/libexec/docker/docker-proxy-current --log-driver=json-file 2>&1 > /tmp/dockerd.log &
-#sudo /usr/bin/dockerd 2>&1 > /dev/null &
+echo
 
 # Determine which virtual env to start at run time
 if [ "$1" == "sshd" ]; then
-sudo /usr/sbin/sshd -D
+    sudo /usr/sbin/sshd -D
 else
-sudo /usr/sbin/sshd
-if [ "$1" == "venv" ] || [ -z "$1" ]; then
-    # interactive
-    cd ~/venv && source bin/activate
-    bash -i
-elif [ "$1" == "toilvenv" ]; then
-    toilvenv
-elif [ "$1" == "_toil_worker" ]; then
-    echo "running _toil_worker [${@:2}]"
-    cd /opt/toil/ && source venv2.7/bin/activate
-    _toil_worker ${@:2}
-    #/usr/bin/_toil_worker "${@:2}"
-elif [ "$1" == "_cwl_worker" ]; then
-    echo "running _cwl_worker [${@:2}]"
-    # run commands in virtualenvironment
-    cd ~/venv && source bin/activate
-    bash -c "${@:2}"
-elif [ "$1" == "_toil_exec" ]; then
-    echo "running _toil_exec"
-    toilvenv "${@:2}"
-elif [ "$1" == "_cwl_exec" ]; then
-    echo "running _cwl_exec"
-    venv "${@:2}"
-fi
+    sudo /usr/sbin/sshd
+    if [ "$1" == "venv" ] || [ -z "$1" ]; then
+        # interactive
+        cd ~/venv && source bin/activate
+        bash -i
+    elif [ "$1" == "toilvenv" ]; then
+        toilvenv
+    elif [ "$1" == "_toil_worker" ]; then
+        echo "running _toil_worker [${@:2}]"
+        cd /opt/toil/ && source venv2.7/bin/activate
+        _toil_worker ${@:2}
+        #/usr/bin/_toil_worker "${@:2}"
+    elif [ "$1" == "_cwl_worker" ]; then
+        echo "running _cwl_worker [${@:2}]"
+        # run commands in virtualenvironment
+        cd ~/venv && source bin/activate
+        bash -c "${@:2}"
+    elif [ "$1" == "_toil_exec" ]; then
+        echo "running _toil_exec"
+        toilvenv "${@:2}"
+    elif [ "$1" == "_cwl_exec" ]; then
+        echo "running _cwl_exec"
+        venv "${@:2}"
+    fi
 fi
 #echo "CALLING SYNC"
-#sync -f /renci/irods
-#sudo umount.davfs /renci/irods/
+sudo umount.davfs ${IRODS_MOUNT}
 #bash -i
